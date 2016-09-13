51c013c1aef7c3d32b5c880e381fbd147d7160bc
Blaxar Waldarax <blaxar.waldarax@gmail.com>
2016-09-13 20:25:36 +0200

    Working on parameters handling.

--- src/libdkcst/dkcst_params.c
+++ src/libdkcst/dkcst_params.c
@@ -11,7 +11,37 @@ DkCst_rc DkCst_create_param_pack(DkCst_params **params) {
 }
 
 DkCst_rc DkCst_set_int_param(DkCst_params *params, char name[MAX_PARAM_NAME_LENGTH], int value) {
-	return NOT_IMPLEMENTED;
+	if(params->first == NULL) { //No param in the list yet
+		params->first = malloc(sizeof(DkCst_param));
+		params->first->type = INT;
+		memcpy(params->first->name, name, MAX_PARAM_NAME_LENGTH);
+		params->first->data.int_value = value;
+		params->first->next = NULL;
+		params->nb_params++;
+		return OK;
+	} else {
+		DkCst_param* prev_param = NULL;
+		DkCst_param* param = params->first;
+		while(param != NULL){ //Iterate over existing params
+			if(strcmp(name, param->name) == 0) { //Param with this name already exists
+				if(param->type == INT) //Update if types do match
+					param->data.int_value = value;
+			    else //Something is wrong, types are differents
+					return ERROR;
+				return OK;
+			}
+			prev_param = param;
+			param=param->next;
+		}
+		//at this point, no existing param with this name, add it to the list.
+		prev_param->next=malloc(sizeof(DkCst_param));
+		prev_param->next->type = INT;
+		memcpy(prev_param->next->name, name, MAX_PARAM_NAME_LENGTH);
+		prev_param->next->data.int_value = value;
+		prev_param->next->next = NULL;
+		params->nb_params++;
+		return OK;
+	}
 }
 
 DkCst_rc DkCst_set_float_param(DkCst_params *params, char name[MAX_PARAM_NAME_LENGTH], float value) {
@@ -23,7 +53,19 @@ DkCst_rc DkCst_set_string_param(DkCst_params *params, char name[MAX_PARAM_NAME_L
 }
 
 DkCst_rc DkCst_get_int_param(DkCst_params *params, char name[MAX_PARAM_NAME_LENGTH], int* value) {
-	return NOT_IMPLEMENTED;
+	DkCst_param* param = params->first;
+	while(param != NULL){ //Iterate over existing params
+		if(strcmp(name, param->name) == 0) { //Param with this name already exists
+			if(param->type == INT) //return value if types do match
+				(*value) = param->data.int_value;
+			else //Something is wrong, types are differents
+				return ERROR;
+			return OK;
+		}
+		param=param->next;
+	}
+	//at this point, no existing param with this name
+	return ERROR;
 }
 
 DkCst_rc DkCst_get_float_param(DkCst_params *params, char name[MAX_PARAM_NAME_LENGTH], float* value) {
@@ -35,7 +77,22 @@ DkCst_rc DkCst_get_string_param(DkCst_params *params, char name[MAX_PARAM_NAME_L
 }
 
 DkCst_rc DkCst_unset_int_param(DkCst_params *params, char name[MAX_PARAM_NAME_LENGTH]) {
-	return NOT_IMPLEMENTED;
+	DkCst_param* prev_param = NULL;
+	DkCst_param* param = params->first;
+	while(param != NULL){ //Iterate over existing params
+		if(strcmp(name, param->name) == 0) { //Param with this name already exists
+			if(param->type == INT) { //remove param if types do match
+				prev_param->next=param->next;
+				free(param);
+			} else //Something is wrong, types are differents
+				return ERROR;
+			return OK;
+		}
+		prev_param = param;
+		param=param->next;
+	}
+	//at this point, no existing param with this name
+	return ERROR;
 }
 
 DkCst_rc DkCst_unset_float_param(DkCst_params *params, char name[MAX_PARAM_NAME_LENGTH]) {
